f5f06e668a8f73aab18f68c47db5cd716fc519ab
Brian Mathiyakom <brian@rarevisions.net>
2017-05-23 02:45:15 -0700

    Refactor with less local vars, more transforms

--- solution.ex
+++ solution.ex
@@ -1,22 +1,23 @@
 defmodule Cipher do
   @alphabet "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
-  def decode(keyword, encoded) do
-    cipher = new_cipher(keyword, @alphabet)
+  defstruct decodebook: %{}
 
+  def decode(cipher, encoded) do
     encoded
     |> String.upcase
     |> String.split("", trim: true)
-    |> Enum.map(fn(e) -> Map.get(cipher, e, " ") end)
+    |> Enum.map(fn(e) -> Map.get(cipher.decodebook, e, " ") end)
     |> Enum.join
   end
 
-  defp new_cipher(keyword, alphabet) do
+  def new(keyword) do
     normalize_keyword(keyword)
     |> normalize_alphabet(@alphabet)
     |> substitution_alphabet
-    |> Enum.zip(String.split(alphabet, "", trim: true))
+    |> Enum.zip(String.split(@alphabet, "", trim: true))
     |> Map.new
+    |> (&%Cipher{decodebook: &1}).()
   end
 
   defp normalize_keyword(keyword) do
@@ -24,40 +25,30 @@ defmodule Cipher do
     |> String.upcase
     |> String.split("", trim: true)
     |> Enum.uniq
-    |> Enum.join
   end
 
   defp normalize_alphabet(normalized_keyword, alphabet) do
-    keyword_list = String.split(normalized_keyword, "", trim: true)
-
-    normalized_alphabet = alphabet
-    |> String.split("")
-    |> Enum.reject(fn(letter) -> Enum.member?(keyword_list, letter) end)
-    |> Enum.join
-
-    {normalized_keyword, normalized_alphabet}
+    alphabet
+    |> String.split("", trim: true)
+    |> Enum.reject(fn(letter) -> Enum.member?(normalized_keyword, letter) end)
+    |> (&{normalized_keyword ++ &1, length(normalized_keyword)}).()
   end
 
-  defp substitution_alphabet({normalized_keyword, normalized_alphabet}) do
-    size = String.length(normalized_keyword)
-
-    String.split(normalized_keyword <> normalized_alphabet, "", trim: true)
-    |> Enum.chunk(size, size, [])
-    |> Enum.map(&fill(&1, size, nil))
+  defp substitution_alphabet({normalized_alphabet, keyword_length}) do
+    normalized_alphabet
+    |> Enum.chunk(keyword_length, keyword_length, [])
+    |> Enum.map(&fill(&1, keyword_length, nil))
     |> transpose
     |> Enum.map(&(Enum.filter(&1, fn(e) -> !is_nil(e) end)))
     |> Enum.sort
     |> List.flatten
   end
 
-  defp fill(list, size, filler) do
-    case length(list) < size do
-      true ->
-        diff = size - length(list)
-        Enum.reduce(1..diff, list, fn(_, acc) -> List.insert_at(acc, -1, filler) end)
-      _ ->
-        list
-    end
+  defp fill(list, size, _) when length(list) >= size, do: list
+  defp fill(list, size, filler) when length(list) < size do
+    Enum.reduce(1..(size - length(list)), list, fn(_, acc) ->
+      List.insert_at(acc, -1, filler)
+    end)
   end
 
   defp transpose(list) do
@@ -73,7 +64,10 @@ defmodule Solution do
     Enum.each(1..n, fn(_) ->
       keyword = IO.gets("") |> String.trim
       encoded = IO.gets("") |> String.trim
-      IO.puts Cipher.decode(keyword, encoded)
+
+      Cipher.new(keyword)
+      |> Cipher.decode(encoded)
+      |> IO.puts
     end)
   end
 end
